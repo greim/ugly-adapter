cea835e39c04df7e36ee0165d10251f6dedcf506
Greg Reimer <gregreimer@gmail.com>
2015-02-17 14:53:59 -0700

    Perf. tweaks.

--- index.js
+++ index.js
@@ -6,26 +6,60 @@
 var Promise = require('native-or-bluebird')
   , slice = Array.prototype.slice
 
-function methodPromify(target, method){
-  var args = slice.call(arguments, 2);
-  return new Promise(function(resolve, reject){
-    args.push(function(err, result){
-      if (err) reject(err);
-      else resolve(result);
+function methodPromify(target, method, arg0, arg1){
+  var argCount = arguments.length - 2;
+  if (argCount > 2){
+    var args = slice.call(arguments, 2);
+    return new Promise(function(resolve, reject){
+      args.push(function(err, result){
+        if (err) reject(err);
+        else resolve(result);
+      });
+      target[method].apply(target, args);
     });
-    target[method].apply(target, args);
-  });
+  } else {
+    return new Promise(function(resolve, reject){
+      var cb = function(err, result){
+        if (err) reject(err);
+        else resolve(result);
+      };
+      if (argCount === 2){
+        target[method].call(target, arg0, arg1, cb);
+      } else if (argCount === 1){
+        target[method].call(target, arg0, cb);
+      } else if (argCount === 0){
+        target[method].call(target, cb);
+      }
+    });
+  }
 }
 
-function functionPromify(fn){
-  var args = slice.call(arguments, 1);
-  return new Promise(function(resolve, reject){
-    args.push(function(err, result){
-      if (err) reject(err);
-      else resolve(result);
+function functionPromify(fn, arg0, arg1){
+  var argCount = arguments.length - 1;
+  if (argCount > 2){
+    var args = slice.call(arguments, 1);
+    return new Promise(function(resolve, reject){
+      args.push(function(err, result){
+        if (err) reject(err);
+        else resolve(result);
+      });
+      fn.apply(null, args);
     });
-    fn.apply(null, args);
-  });
+  } else {
+    return new Promise(function(resolve, reject){
+      var cb = function(err, result){
+        if (err) reject(err);
+        else resolve(result);
+      };
+      if (argCount === 2){
+        fn.call(null, arg0, arg1, cb);
+      } else if (argCount === 1){
+        fn.call(null, arg0, cb);
+      } else if (argCount === 0){
+        fn.call(null, cb);
+      }
+    });
+  }
 };
 
 module.exports = functionPromify;
--- test/tests.js
+++ test/tests.js
@@ -36,49 +36,95 @@ function arrayify(){
   })
 }
 
-describe(pkg.name, function(){
 
-  describe('test functions', function(){
+describe('test helpers', function(){
 
-    it('should say()', function(done){
+  it('should say()', function(done){
 
-      say('hello', function(err, message){
-        if (err) done(err)
-        assert.strictEqual(message, 'hello')
-        done()
-      })
-    })
-
-    it('should getSelf()', function(done){
-
-      var thisThing = {}
-      getSelf.call(thisThing, function(err, thatThing){
-        if (err) done(err)
-        assert.strictEqual(thisThing, thatThing)
-        done()
-      })
-    })
-
-    it('should fail()', function(done){
-
-      var thisThing = {}
-      fail(function(err){
-        assert.ok(err)
-        done()
-      })
-    })
-
-    it('should arrayify()', function(done){
-
-      arrayify(1,2,3,function(err, array){
-        if (err) done(err)
-        assert.deepEqual(array, [1,2,3])
-        done()
-      })
+    say('hello', function(err, message){
+      if (err) done(err)
+      assert.strictEqual(message, 'hello')
+      done()
     })
   })
 
-  describe('functions', function(){
+  it('should getSelf()', function(done){
+
+    var thisThing = {}
+    getSelf.call(thisThing, function(err, thatThing){
+      if (err) done(err)
+      assert.strictEqual(thisThing, thatThing)
+      done()
+    })
+  })
+
+  it('should fail()', function(done){
+
+    var thisThing = {}
+    fail(function(err){
+      assert.ok(err)
+      done()
+    })
+  })
+
+  it('should arrayify() 0 args', function(done){
+
+    arrayify(function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [])
+      done()
+    })
+  })
+
+  it('should arrayify() 1 arg', function(done){
+
+    arrayify(1,function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [1])
+      done()
+    })
+  })
+
+  it('should arrayify() 2 args', function(done){
+
+    arrayify(1,2,function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [1,2])
+      done()
+    })
+  })
+
+  it('should arrayify() 3 args', function(done){
+
+    arrayify(1,2,3,function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [1,2,3])
+      done()
+    })
+  })
+
+  it('should arrayify() 4 args', function(done){
+
+    arrayify(1,2,3,4,function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [1,2,3,4])
+      done()
+    })
+  })
+
+  it('should arrayify() falsy args', function(done){
+
+    arrayify(null, undefined, false,function(err, array){
+      if (err) done(err)
+      assert.deepEqual(array, [null, undefined, false])
+      done()
+    })
+  })
+})
+
+describe(pkg.name, function(){
+
+  describe('bare functions', function(){
 
     it('should work', function(done){
 
@@ -95,7 +141,7 @@ describe(pkg.name, function(){
       assert.ok(p.then && typeof p.then === 'function', 'not a promise')
     })
 
-    it('should pass context', function(done){
+    it('should pass global context', function(done){
 
       co(function*(){
         var self = yield adapt(getSelf)
@@ -104,11 +150,56 @@ describe(pkg.name, function(){
       }).catch(done)
     })
 
-    it('should pass arguments', function(done){
+    it('should pass 0 arguments', function(done){
+
+      co(function*(){
+        var array = yield adapt(arrayify)
+        assert.deepEqual(array, [])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 1 argument', function(done){
+
+      co(function*(){
+        var array = yield adapt(arrayify, 'a')
+        assert.deepEqual(array, ['a'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 2 arguments', function(done){
+
+      co(function*(){
+        var array = yield adapt(arrayify, 'a', 'b')
+        assert.deepEqual(array, ['a','b'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 3 arguments', function(done){
 
       co(function*(){
         var array = yield adapt(arrayify, 'a', 'b', 'c')
         assert.deepEqual(array, ['a','b','c'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 4 arguments', function(done){
+
+      co(function*(){
+        var array = yield adapt(arrayify, 'a', 'b', 'c', 'd')
+        assert.deepEqual(array, ['a','b','c','d'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass falsy arguments', function(done){
+
+      co(function*(){
+        var array = yield adapt(arrayify, null, undefined, false)
+        assert.deepEqual(array, [null, undefined, false])
         done()
       }).catch(done)
     })
@@ -124,7 +215,7 @@ describe(pkg.name, function(){
     })
   })
 
-  describe('methods', function(){
+  describe('methods on objects', function(){
 
     it('should work', function(done){
 
@@ -153,12 +244,62 @@ describe(pkg.name, function(){
       }).catch(done)
     })
 
-    it('should pass arguments', function(done){
+    it('should pass 0 arguments', function(done){
+
+      co(function*(){
+        var fakeLib = { arrayify: arrayify }
+        var array = yield adapt.method(fakeLib, 'arrayify')
+        assert.deepEqual(array, [])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 1 argument', function(done){
+
+      co(function*(){
+        var fakeLib = { arrayify: arrayify }
+        var array = yield adapt.method(fakeLib, 'arrayify', 'a')
+        assert.deepEqual(array, ['a'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 2 arguments', function(done){
+
+      co(function*(){
+        var fakeLib = { arrayify: arrayify }
+        var array = yield adapt.method(fakeLib, 'arrayify', 'a', 'b')
+        assert.deepEqual(array, ['a','b'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 3 arguments', function(done){
 
       co(function*(){
         var fakeLib = { arrayify: arrayify }
         var array = yield adapt.method(fakeLib, 'arrayify', 'a', 'b', 'c')
         assert.deepEqual(array, ['a','b','c'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass 4 arguments', function(done){
+
+      co(function*(){
+        var fakeLib = { arrayify: arrayify }
+        var array = yield adapt.method(fakeLib, 'arrayify', 'a', 'b', 'c', 'd')
+        assert.deepEqual(array, ['a','b','c','d'])
+        done()
+      }).catch(done)
+    })
+
+    it('should pass falsy arguments', function(done){
+
+      co(function*(){
+        var fakeLib = { arrayify: arrayify }
+        var array = yield adapt.method(fakeLib, 'arrayify',null,undefined,false)
+        assert.deepEqual(array, [null,undefined,false])
         done()
       }).catch(done)
     })
